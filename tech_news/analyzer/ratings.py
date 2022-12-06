5062df23e1fe8af0b38aa6f50085089e79a09329
AugustoMagalhaes <augustommg@gmail.com>
2022-12-05 21:15:31 -0300

    feat: implementa top_5_news.

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,6 +1,17 @@
+from tech_news.database import get_collection
+
+
 # Requisito 10
 def top_5_news():
-    """Seu código deve vir aqui"""
+    top_5_data = list(get_collection().find(
+        {}, {"title": True, "url": True})
+        .sort("comments_count", - 1).limit(5))
+    top_5_commented = []
+
+    for data in top_5_data:
+        top_5_commented.append((data["title"], data["url"]))
+
+    return top_5_commented
 
 
 # Requisito 11
