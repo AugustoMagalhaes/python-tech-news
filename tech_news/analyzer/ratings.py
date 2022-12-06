80e73544f59f9d46fcebf1b8026c135cf719cca3
AugustoMagalhaes <augustommg@gmail.com>
2022-12-06 00:18:57 -0300

    feat: implementa top_5_categories.

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -14,6 +14,33 @@ def top_5_news():
     return top_5_commented
 
 
+def top_5_ordered(dict):
+    ordered_elements = sorted(
+      dict.items(), key=lambda el: el[1], reverse=True)
+    result = list(map(lambda el: el[0], ordered_elements[:5]))
+
+    return result
+
+
+def count_list_elements(dict_list):
+    counted_elements = {}
+    values_list = list(map(lambda el: el["category"], dict_list))
+
+    for item in values_list:
+        if item in counted_elements.keys():
+            counted_elements[item] += 1
+        else:
+            counted_elements[item] = 1
+
+    return top_5_ordered(counted_elements)
+
+
 # Requisito 11
 def top_5_categories():
-    """Seu código deve vir aqui"""
+    top_5_categories = list(get_collection().find(
+        {}, {"category": True, "_id": False})
+        .sort("category", 1))
+
+    ordered_categories_count = count_list_elements(top_5_categories)
+
+    return ordered_categories_count
